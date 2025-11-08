f42079a8b657aa76d464f3d71e85aa14a6e7587a
miho daijo <mihodaijo@mihonoMacBook-Air.local>
2025-11-08 20:13:11 +0900

    変更6

--- chapter0/iam_github_oidc.tf
+++ chapter0/iam_github_oidc.tf
@@ -29,6 +29,9 @@ resource "aws_iam_openid_connect_provider" "github" {
   client_id_list = ["sts.amazonaws.com"]
   # GitHub OIDC のルートCAフィンガープリント（ATS）
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 ########################################
