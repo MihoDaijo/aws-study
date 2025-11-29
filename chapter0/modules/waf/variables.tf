088679a6786ae305b974f76a21d4af3fea4d4739
miho daijo <mihodaijo@mihonoMacBook-Air.local>
2025-11-29 21:00:26 +0900

    “変更11/29”

--- chapter0/modules/waf/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  description = "Name of the WAF Web ACL"
-  type        = string
-}
-
-variable "resource_arn" {
-  description = "ARN of the resource to associate with WAF"
-  type        = string
-}
